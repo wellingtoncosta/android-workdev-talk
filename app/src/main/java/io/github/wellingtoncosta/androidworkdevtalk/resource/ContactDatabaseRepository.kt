0a0e59102a3e68fa3c3a6e6fc78b3a150e8d6132
Wellington Costa <wellington.costa128@gmail.com>
2019-04-27 10:06:12 -0300

    Creating a list of fake contacts.

--- app/src/main/java/io/github/wellingtoncosta/androidworkdevtalk/resource/ContactDatabaseRepository.kt
+++ app/src/main/java/io/github/wellingtoncosta/androidworkdevtalk/resource/ContactDatabaseRepository.kt
@@ -7,6 +7,20 @@ class ContactDatabaseRepository : ContactRepository {
 
     private val contacts = ArrayList<Contact>()
 
+    init {
+        contacts.addAll(listOf(
+            Contact(1, "Lorrie Potts", "lorrie.potts@email.com", "+1 (848) 573-2104"),
+            Contact(2, "Wade Nieves", "wade.nieves@email.com", "+1 (961) 527-2685"),
+            Contact(3, "Barry Vang", "barry.vang@email.com", "+1 (991) 489-3025"),
+            Contact(4, "Snider Allen", "snider.allen@email.com", "+1 (826) 574-2349"),
+            Contact(5, "Silva Finley", "silva.finley@email.com", "+1 (887) 409-3362"),
+            Contact(6, "Pierce Leonard", "pierce.leonard@email.com", "+1 (872) 459-2172"),
+            Contact(7, "Eliza Davis", "eliza.davis@email.com", "+1 (845) 583-3489"),
+            Contact(8, "Vicky Knapp", "vicky.knapp@email.com", "+1 (844) 475-3584"),
+            Contact(9, "Nancy Hancock", "nancy.hancock@email.com", "+1 (869) 535-2431")
+        ))
+    }
+
     override fun findAll() = contacts
 
     override fun findById(contactId: Long) = contacts.firstOrNull { it.id == contactId }
